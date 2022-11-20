2c8959d5db0baa9b3bcc98dd946fb587e6a7431c
Alwin Low <alow@ucsd.edu>
2022-11-20 04:01:01 -0800

    expose and explore finished

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Lab 8 - Starter
+1. Within a Github action that runs whenever code is pushed. This option is better since the tests will be automatically run and does not require our attention so we can focus on other things that warrants our concern.
+2. No
+3. No I would not. The message feature is a core component of the application and involves many moving parts. Unit tests are unable to test how individual components interact with each other on a feature level. Not to mention modifying the message feature would also affect other unit tests.
+4. Yes I would. Testing for max message length is on a much smaller scale and does not involve many moving parts. Modifying this feature would also not have much of an effect on other non-related unit tests.
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -3,3 +3,100 @@
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
 // TODO - Part 2
+//isPhoneNumber True Cases
+test('(669) 306-2484 is a valid phone number', () => {
+    expect(functions.isPhoneNumber('(669) 306-2484')).toBe(true);  
+});
+
+test('(418) 326-7884 is a valid phone number', () => {
+    expect(functions.isPhoneNumber('(418) 326-7884')).toBe(true);  
+});
+
+//isPhoneNumber False Cases
+test('46915207891 is not a valid phone number', () => {
+    expect(functions.isPhoneNumber('46915207891')).toBe(false);  
+});
+
+test('Alphabets is not a valid phone number', () => {
+    expect(functions.isPhoneNumber('Alphabets')).toBe(false);  
+});
+
+//**************************************************************
+
+//isEmail True Cases
+test('alwlow@hotmail.com is a valid email', () => {
+    expect(functions.isEmail('alwlow@hotmail.com')).toBe(true);  
+});
+
+test('true@gmail.com is a valid email', () => {
+    expect(functions.isEmail('true@gmail.com')).toBe(true);  
+});
+
+//isEmail False Cases
+test('alwlow@hotmail is not a valid email', () => {
+    expect(functions.isEmail('alwlow@hotmail')).toBe(false);  
+});
+  
+test('$@**^@#*!)()@gmail.com is not a valid email', () => {
+    expect(functions.isEmail('$@**^@#*!)()@gmail.com')).toBe(false);  
+});
+
+//**************************************************************
+
+//isStrongPassword True Cases
+test('c_1r is a valid strong password', () => {
+    expect(functions.isStrongPassword('c_1r')).toBe(true);  
+});
+
+test('are1_tsna23_raw is a valid strong password', () => {
+    expect(functions.isStrongPassword('are1_tsna23_raw')).toBe(true);  
+});
+
+//isStrongPassword False Cases
+test('1_ecrasd is not a valid strong password', () => {
+    expect(functions.isStrongPassword('1_ecrasd')).toBe(false);  
+});
+
+test('are1_tsna23_rawadsa_131adada is not a valid strong password', () => {
+    expect(functions.isStrongPassword('are1_tsna23_rawadsa_131adada')).toBe(false);  
+});
+
+//**************************************************************
+
+//isDate True Cases
+test('11/20/2022 is a valid date', () => {
+    expect(functions.isDate('11/20/2022')).toBe(true);  
+});
+
+test('11/20/2022 is a valid date', () => {
+    expect(functions.isDate('1/2/2002')).toBe(true);  
+});
+
+//isDate False Cases
+test('11/20/22222 is not a valid date', () => {
+    expect(functions.isDate('11/20/22222')).toBe(false);  
+});
+
+test('aa/bb/cccc is not a valid date', () => {
+    expect(functions.isDate('aa/bb/cccc')).toBe(false);  
+});
+
+//**************************************************************
+
+//isHexColor True Cases
+test('#FFF is a valid hex color', () => {
+    expect(functions.isHexColor('#FFF')).toBe(true);  
+});
+
+test('#FF5733 is a valid hex color', () => {
+    expect(functions.isHexColor('#FF5733')).toBe(true);  
+});
+
+//isHexColor False Cases
+test('AA11AA1 is not a valid hex color', () => {
+    expect(functions.isHexColor('AA11AA1')).toBe(false);  
+});
+
+test('#FF5733 is not a valid hex color', () => {
+    expect(functions.isHexColor('#FF89')).toBe(false);  
+});
